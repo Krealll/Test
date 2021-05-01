cc593e10e511603cd268df02df3fb8d7254b76a9
Krealll <krealll@mail.ru>
2021-05-02 02:14:33 +0300

    Test 51

--- src/main/java/com/example/krealll/CustomClass.java
+++ src/main/java/com/example/krealll/CustomClass.java
@@ -2,21 +2,24 @@ package com.example.krealll;
 
 public class CustomClass {
 
-    public boolean isMoreThanThree(int n){
-        return n>3;
-        //Usefull content
-        //Usefull content
-        //Usefull content
-        //Usefull content
-        //Usefull content
-        //Usefull content
-        //Usefull content
-        //Usefull content
-        //Usefull content
-        //Usefull content
-        //Usefull content
+    public boolean isPalindrome(String inputString) {
+        if (inputString.length() == 0) {
+            return true;
+        } else {
+            String rev = "";
+            int length = inputString.length();
 
+            for ( int i = length - 1; i >= 0; i-- )
+                rev = rev + inputString.charAt(i);
+
+            if (inputString.equals(rev))
+                return true;
+            else
+                return false;
+        }
     }
+
+
 
 
 }
--- src/test/java/com/example/krealll/CustomClassTest.java
+++ src/test/java/com/example/krealll/CustomClassTest.java
@@ -1,52 +1,17 @@
 package com.example.krealll;
 
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class CustomClassTest {
 
-    private CustomClass customClass;
-    private  int one;
-    private  int two;
-    private  int five;
-    private int four;
-    private int six;
-
-    @BeforeClass
-    void setUp() {
-        customClass = new CustomClass();
-        one=1;
-        two=2;
-        five=5;
-        six=6;
-        four=4;
-    }
-
     @Test
-    void isMoreThanThreePositiveTest() {
-        assertTrue(customClass.isMoreThanThree(five));
-    }
-
-    @Test
-    void isMoreThanThreePositiveTwoTest() {
-        assertTrue(customClass.isMoreThanThree(four));
-    }
-
-    @Test
-    void isMoreThanThreePositiveThreeTest() {
-        assertTrue(customClass.isMoreThanThree(six));
-    }
-
-    @Test
-    void isMoreThanThreeNegativeTest() {
-        assertFalse(customClass.isMoreThanThree(one));
-    }
-
-    @Test
-    void isMoreThanThreeNegativeTwoTest() {
-        assertFalse(customClass.isMoreThanThree(two));
+    public void whenEmptyString_thenAccept() {
+        CustomClass palindromeTester = new CustomClass();
+        assertTrue(palindromeTester.isPalindrome(""));
     }
 
 }
